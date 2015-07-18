3d4e2aa5a0aca87f55ef20b62130f8117a2a0e9d
jooyoung91 <jooyoung91@gmail.com>
2015-07-18 15:14:04 -0700

    initial commit

---
+++ AngularJSMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AngualMVC
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/AwesomeAngularMVCApp")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/AwesomeAngularMVCApp.js")); 
+            
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ AngularJSMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngualMVC.MvcApplication" Language="C#" %>
